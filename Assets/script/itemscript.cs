12637d5438359dcf428cca9bc019c6f42ed1a96e
ganmo283 <kikuofive@yahoo.co.jp>
2020-09-13 14:05:03 +0900

    アイテムボックスにスクリプト追加

---
+++ Assets/script/itemscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemscript : MonoBehaviour
+{
+    Rigidbody rb;
+    public int RocketCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            RocketCount++;
+            Debug.Log("s");
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/script/rocketscript.cs
+++ Assets/script/rocketscript.cs
@@ -8,17 +8,23 @@ public class rocketscript : MonoBehaviour
     public float shotSpeed;
     private float timeBetweenShot=1f;
     public float timer;
+    public int RocketCount;
+    public itemscript itemScript;
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
         if (Input.GetKey(KeyCode.Space) && timer > timeBetweenShot)
         {
-            timer = 0.0f;
-            GameObject Rocket = Instantiate(RocketPrefab, transform.position, RocketPrefab.transform.rotation);
-            Rigidbody RocketRb = Rocket.GetComponent<Rigidbody>();
-            RocketRb.AddForce(transform.forward * shotSpeed);
-            Destroy(Rocket, 3.0f);
+            if (RocketCount > 0)
+            {
+                timer = 0.0f;
+                GameObject Rocket = Instantiate(RocketPrefab, transform.position, RocketPrefab.transform.rotation);
+                Rigidbody RocketRb = Rocket.GetComponent<Rigidbody>();
+                RocketRb.AddForce(transform.forward * shotSpeed);
+                Destroy(Rocket, 3.0f);
+                RocketCount--;
+            }
         }
     }
 }
